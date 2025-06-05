e5bf73609e6e08ecda58ac9e96928c66d71fc7f5
Mirage <138711762+Mirage042@users.noreply.github.com>
2025-06-06 00:53:37 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'client/src'),
+      "@": path.resolve(__dirname, "client/src"),
     },
   },
-  root: path.resolve(__dirname, 'client'),
+  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, 'dist/public'),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
   esbuild: {
-    loader: 'tsx',
+    loader: "tsx",
     include: /src\/.*\.[tj]sx?$/,
-    exclude: []
+    exclude: [],
   },
   optimizeDeps: {
-    include: ['vue', 'vue-router', 'vuetify']
-  }
-})
+    include: ["vue", "vue-router", "vuetify"],
+  },
+});
